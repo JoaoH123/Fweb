dfd7049055fbfac1adf5d9c7ae7b9a1fae2afa3a
JoaoH123 <joaoh220708@gmail.com>
2024-07-29 21:38:55 -0300

    Add files via upload

--- Aulas/Aula 12/calculadora.js
+++ Aulas/Aula 12/calculadora.js
@@ -12,20 +12,25 @@ function addDecimal(){
     uptadeText();
 }
 
+//comando para anexar os numeros na calculadora//
 function uptadeText(text){
     var numero = document.getElementById('texto').innerHTML;
     document.getElementById('texto').innerHTML = numero + text
 }
 
+//comando para limpar a calculadora//
 function clearText(){
     document.getElementById('texto').innerHTML = ""
 }
 
+//comando para limpar apenas 1 numero//
 function returnText(){
     var resultado = document.getElementById('texto').innerHTML;
     document.getElementById('texto').innerHTML = resultado.substring(0, resultado.length -1)
 }
 
+
+//comando para calcular os numeros//
 function calculate(){
     var resultado = document.getElementById('texto').innerHTML;
     if(resultado){
---
+++ Aulas/Aula 15/exer2/exer2.js
@@ -0,0 +1,3 @@
+$('.parallax-window').parallax({imageSrc: './img/6202b931f3d654284e53d6a3e8616a4a.jpg'});
+
+jQuery(window).trigger('resize').trigger('scroll');
